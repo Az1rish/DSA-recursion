d9d093d40287eb41c4eb7bfb05f8f8239cf925d9
Shane <shanemcneil19@gmail.com>
2020-03-30 07:31:03 -0700

    complete fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+const fibonacci = function(number) {
+    if (number <= 2) {
+      return 1
+    }
+    
+    return fibonacci(number - 1) + fibonacci(number - 2)
+  }
+  
+  console.log(fibonacci(7))
